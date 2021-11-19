133c6d7609058ec534ebe1b73dbc219dce12f4fc
Ashish <ashishdhola0.7@gmail.com>
2021-11-19 15:32:31 +0530

    secondcommit

---
+++ 2name.js
@@ -0,0 +1,20 @@
+window.onload = function ()
+{
+    const btn = document.getElementById("btn");
+    btn.onclick = function ()
+    {
+        let mytext=document.getElementById("Fname").value;    
+        let mytext1=document.getElementById("Lname").value;      
+        if (mytext==mytext1)
+        {
+            document.getElementById("myMessage").innerHTML = "your name is same";
+               
+        }
+        else
+        {
+          
+            document.getElementById("myMessage").innerHTML = "your name is valid for continue";
+            document.getElementById("name").value=mytext+" "+mytext1;
+        }
+    }
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+window.onload = function ()
+{
+    const btn = document.getElementById("btn");
+    btn.onclick = function ()
+    {
+        let mytext=document.getElementById("js").value;    
+        
+    }        
